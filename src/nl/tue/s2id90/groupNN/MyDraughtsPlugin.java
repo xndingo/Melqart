519a8ec70eef2478649398aa41c3dd6ab0151760
jvhoof <jvhoof@localhost.localdomain>
2014-02-07 23:36:02 +0100

    Set up some classes required for Section 2.

--- src/nl/tue/s2id90/groupNN/MyDraughtsPlugin.java
+++ src/nl/tue/s2id90/groupNN/MyDraughtsPlugin.java
@@ -18,6 +18,7 @@ public class MyDraughtsPlugin extends DraughtsPlugin {
         // During the final competition you should make only your 
         // best player available. For testing it might be handy
         // to make more than one player available.
-        super(new UninformedPlayer(), new OptimisticPlayer());
+        //super(new UninformedPlayer(), new OptimisticPlayer());
+        super(new UninformedPlayer(), new MelqartPlayer());
     }
 }
