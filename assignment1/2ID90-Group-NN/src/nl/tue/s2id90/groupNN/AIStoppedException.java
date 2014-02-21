40f4c7028970157e5fd1d72df3bae56d261b1b3a
root <root@localhost.localdomain>
2014-02-21 17:32:20 +0100

    Merged old stuff together.

---
+++ assignment1/2ID90-Group-NN/src/nl/tue/s2id90/groupNN/AIStoppedException.java
@@ -0,0 +1,14 @@
+ 
+package nl.tue.s2id90.groupNN;
+
+/** 
+ * Special exception to stop the alpha-beta method call if it takes too long.
+ * @author Jeroen van Hoof
+ * @author Theodore Margomenos
+ */
+class AIStoppedException extends Exception {
+
+    public AIStoppedException() {
+    }
+    
+}
