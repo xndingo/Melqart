e3d0ef84be024ac61e72e2de77e53afb68d02f14
Theodore Margomenos <xndingo@outlook.com>
2014-02-26 12:37:30 +0100

    alpha beta working properly on our player

--- assignment1/2ID90-Group-NN/src/nl/tue/s2id90/groupNN/MelqartPlayer.java
+++ assignment1/2ID90-Group-NN/src/nl/tue/s2id90/groupNN/MelqartPlayer.java
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
 package nl.tue.s2id90.groupNN;
 
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import nl.tue.s2id90.draughts.Draughts;
 import nl.tue.s2id90.draughts.DraughtsState;
 import nl.tue.s2id90.draughts.player.DraughtsPlayer;
@@ -16,122 +17,11 @@ import org10x10.dam.game.Move;
  * @author Theodoros Margomenos
  */
 public class MelqartPlayer extends DraughtsPlayer {
-
-    private Move bestMove = null;
-    private int value = 0;
+    private final int maxDepth = 6;
+    private int value;
+    private int count;
     
-    /**
-     * Returns the amount of pieces that are on the board given a game state
-     * {@code gs}
-     *
-     * @param gs game state of the board
-     * @return the amount of pieces that are on the board. 0 if empty.
-     */
-    public int getPieceCount(DraughtsState gs) {
-        int[] pieces = gs.getPieces();
-        int count = 0;
-        for (int f = 1; f < pieces.length; f = f + 1) {
-            int piece = pieces[f];
-            if (Draughts.isWhite(piece) || Draughts.isBlack(piece)) {
-                count++;
-            }
-        }
-        return count; //0 if no pieces on the board.
-    }
-
-    private boolean stopped = false;
-
-    @Override
-    public void stop() {
-        stopped = true;
-    }
-
-int alphaBeta (GameNode node, int depth, int alpha, int beta, boolean maximizingPlayer){
-        DraughtsState state = node.getState();
-        List<Move> moves = state.getMoves();
-        if (depth == 0 || state.isEndState()) {
-            return node.getValue(state);
-        } 
-        if (maximizingPlayer) {
-            for (Move move : moves){
-                state.doMove(move);
-                // recursive call
-                System.out.println("Clone");
-                GameNode newNode = new GameNode(state.clone());
-                System.out.println("End clone");
-                alpha = max(alpha, alphaBeta(newNode, depth-1, alpha, beta, false));
-                if (beta <= alpha) {
-                    this.bestMove = move;
-                    break; //b cut off
-                }
-                state.undoMove(move);                                
-            }
-            //node.setBestMove(alpha);
-            
-            return alpha;
-        }
-        else { //minimizingPlayer
-            for (Move move : moves){
-                state.doMove(move);
-                // recursive call
-                System.out.println("Clone");
-                GameNode newNode = new GameNode(state.clone());
-                System.out.println("End clone");
-                beta = min(beta, alphaBeta(newNode, depth-1, alpha, beta, true));
-                if (beta <= alpha) {
-                    this.bestMove = move;
-                    break; //a cut off
-                }
-                state.undoMove(move);                     
-            }
-            //node.setBestMove(beta);
-            
-            return beta; 
-        }
-}
-
-    @Override
-    public Move getMove(DraughtsState ds) {
-        if (ds == null){
-            throw new IllegalArgumentException("blub");
-        } // Not ze problem. le baguette
-        System.out.println("getmoves");
-        List<Move> moves = ds.getMoves();
-        System.out.println("gamenode");
-        GameNode node = new GameNode(ds.clone());
-        System.out.println("alphaBeta");
-        this.value = alphaBeta(node, 1, 0, 0, ds.isWhiteToMove());
-        if (this.bestMove == null){
-            throw new IllegalArgumentException("blub2");
-        } // Not ze problem. le baguette
-        System.out.println("The return: extension, remastered");
-        return this.bestMove;
-    }
     
-    @Override
-    public Integer getValue() {
-        return value;
-    }
-}
-=======
-package nl.tue.s2id90.groupNN;
-
-import static java.lang.Math.max;
-import static java.lang.Math.min;
-import java.util.Collections;
-import java.util.List;
-import nl.tue.s2id90.draughts.Draughts;
-import nl.tue.s2id90.draughts.DraughtsState;
-import nl.tue.s2id90.draughts.player.DraughtsPlayer;
-import nl.tue.s2id90.game.GameState;
-import org10x10.dam.game.Move;
-
-/**
- * @author Jeroen van Hoof
- * @author Theodoros Margomenos
- */
-public class MelqartPlayer extends DraughtsPlayer {
-
     public MelqartPlayer() {
         super(MelqartPlayer.class.getResource("resources/squirtle.jpg"));
     }
@@ -161,56 +51,62 @@ public class MelqartPlayer extends DraughtsPlayer {
     public void stop() {
         stopped = true;
     }
-
-int alphaBeta (GameNode node, int depth, int alpha, int beta, boolean maximizingPlayer){
-        GameState state = node.getState();
-        List<Move> moves = state.getMoves();
-        if (depth == 0 || state.isEndState()) {
-            return node.getValue();
-        } 
-        if (maximizingPlayer) {
-            for (Move move : moves){
-                state.doMove(move);
-                // recursive call
-                alpha = max(alpha, alphaBeta((GameNode) state, depth-1, alpha, beta, false));
-                if (beta <= alpha) {
-                    break; //b cut off
-                }
-                state.undoMove(move);                                
-            }
-            node.setBestMove(alpha);
-            return alpha;           
+   
+    int alphaBeta(GameNode node, int alpha, int beta, int player, int depth) 
+            throws Exception{
+        count++;
+        DraughtsState state = node.getState();
+        if (depth == 0){
+            return player * node.getValue(node.getState());
         }
-        else { //minimizingPlayer
-            for (Move move : moves){
-                state.doMove(move);
-                // recursive call
-                beta = min(beta, alphaBeta((GameNode) state, depth-1, alpha, beta, true));
-                if (beta <= alpha) {
-                    break; //a cut off
-                }
-                state.undoMove(move);                     
+        for (Move move : state.getMoves()){
+            state.doMove(move);
+            alpha = max(alpha, -alphaBeta(new GameNode(state.clone()), 
+                    -beta, -alpha, -(player), depth - 1));
+            state.undoMove(move);
+            if (beta >= alpha){
+                return alpha;
             }
-            node.setBestMove(beta);
-            return beta; 
         }
-}
+        return alpha;
+    }
+    
+    Move rootAlphaBeta(GameNode node, int alpha, int beta, int player, int depth) 
+            throws Exception {
+        Move bestMove = null;
+        DraughtsState state = node.getState();
+        int maxEval = -10000;
+        for (Move move : state.getMoves()){
+            state.doMove(move);
+            alpha = max(alpha, -alphaBeta(new GameNode(state.clone()), 
+                    -beta, -alpha, -(player), depth - 1));
+            state.undoMove(move);
+            if (alpha > maxEval){
+                maxEval = alpha;
+                bestMove = move;
+            }
+        }
+        System.out.println("Count:" + count);
+        count = 0;
+        return bestMove;
+    }
 
     @Override
-    public Move getMove(DraughtsState s) {
-        System.out.println("getmoves");
-        List<Move> moves = s.getMoves();
-        System.out.println("gamenode");
-        GameNode node = new GameNode(s.clone());
-        System.out.println("alphaBeta");
-        alphaBeta(node, 1, 0, 0, true);
-        System.out.println("return");
-        return moves.get(0);
+    public Move getMove(DraughtsState ds) {
+        try {
+            List<Move> moves = ds.getMoves();
+            GameNode node = new GameNode(ds.clone());
+            
+            return rootAlphaBeta(node, -10000, 10000, 1, this.maxDepth);
+        } catch (Exception ex) {
+            Logger.getLogger(MelqartPlayer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
     }
     
     @Override
     public Integer getValue() {
-        return 1337;
+        return this.value;
     }
 }
->>>>>>> 970d8073ce6df65a8e99280c6e64df9278cabf0d
